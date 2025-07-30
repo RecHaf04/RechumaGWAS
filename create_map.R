7d2f7926bbb43e34cfa91687d7cae3fa6391ac5d
RecHaf04 <rechuma.s.hafter@gmail.com>
2025-07-30 14:40:29 -0400

    Update create_map.R 7/30/25

--- create_map.R
+++ create_map.R
@@ -1,21 +1,37 @@
 library(dplyr)
+library(tibble)
+
 
 chr_lengths <- data.frame(
   Chromosome = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", 
                  "11", "12", "13", "14", "15", "16", "17", "18", "19", 
                  "20", "21", "22", "X", "Y"),
-  chr_len = c(248956422, 242193529, 198295559, 190214555, 181538259, 
+  Length = c(248956422, 242193529, 198295559, 190214555, 181538259, 
               170805979, 159345973, 145138636, 138394717, 133797422,
               135086622, 133275309, 114364328, 107043718, 101991189,
               90338345, 83257441, 80373285, 58617616, 64444167,
               46709983, 50818468, 156040895, 57227415)
 )
-chr_map <- chr_lengths %>% 
-  mutate(Chr_numeric = as.numeric(factor(Chromosome, levels = c(1:22, "X", "Y")))) %>%
-  arrange(Chr_numeric) %>%
-  mutate(total = lag(cumsum(as.numeric(chr_len)), default = 0))
+chr_lengths$Chromosome <- factor(chr_lengths$Chromosome, levels = c(as.character(1:22), "X", "Y"))
+chr_lengths <- chr_lengths[order(chr_lengths$Chromosome), ]
 
-# --- Save the new, correct map file ---
-saveRDS(chr_map, "chr_map.rds")
+# Create the map with precise, non-overlapping boundaries
+chr_map <- chr_lengths %>%
+  mutate(cumulative_end = cumsum(as.numeric(Length))) %>%
+  mutate(cumulative_start = lag(cumulative_end, default = 0) + 1) %>%
+  # Correct the start of the first chromosome to be 0
+  mutate(cumulative_start = if_else(Chromosome == "1", 0, cumulative_start))
 
-print("New chr_map.rds file created successfully.")
+# The total length is simply the end of the last chromosome
+total_genome_length <- max(chr_map$cumulative_end)
+
+# Save the final, correct map
+saveRDS(
+  list(
+    chr_map = chr_map,
+    total_genome_length = total_genome_length
+  ), 
+  file = "chr_map_data.rds"
+)
+
+print("Final, precise chr_map_data.rds file created successfully. ✅")
